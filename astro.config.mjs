470d72741035f811e08f5dd7713a32bef2a50611
Yuanzi0721 <230116834@qq.com>
2026-06-12 00:11:45 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -52,7 +52,7 @@ const adapter = process.env.CF_WORKERS
 
 // https://astro.build/config
 export default defineConfig({
-	site: siteConfig.site_url,
+	site:"https://Yuanzi0721.github.io",
 
 	base: "/",
 	trailingSlash: "always",
